7cd8e6bde93f0d1f917fea74869863623abde421
Mary Nazareno <47845955+mgracnazareno@users.noreply.github.com>
2023-12-28 16:43:24 -0500

    display user name & role

--- src/main/java/com/projet/agenda/security/SecurityConfig.java
+++ src/main/java/com/projet/agenda/security/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.projet.agenda.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -30,5 +33,23 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                    configurer
+                            .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                       form
+
+                        .loginPage("/showLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll
+                );
+        return http.build();
     }
 }
